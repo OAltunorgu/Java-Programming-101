43bf52957df294794c585780d156cde0e0cdc46a
WHOREU <WHOREU@DESKTOP-NQ57GQJ>
2021-05-08 11:07:41 +0300

    02-variables

---
+++ 02-variables/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		//Deðiþkenler
+		System.out.println("Sayi-1 : 10");
+		
+		int num = 10;
+		String mesaj = "Sayi-2 : ";
+		System.out.println(mesaj + num);
+	}
+}
