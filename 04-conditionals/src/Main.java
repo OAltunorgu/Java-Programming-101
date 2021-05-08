f08d5c906859b1fc6099addd695bdf5017836a16
WHOREU <WHOREU@DESKTOP-NQ57GQJ>
2021-05-08 11:08:29 +0300

    04-conditionals

---
+++ 04-conditionals/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int notunuz = 50;
+		if (notunuz<50) {
+			System.out.println("Notunuz ders geçme notunun altýnda kaldý, notunuz: " + notunuz);
+		}else if (notunuz == 50) {
+			System.out.println("Notunuz ders geçme notuna eþittir, notunuz: " + notunuz);
+		}else {
+			System.out.println("Notunuz ders geçme notundan büyüktür, notunuz: " + notunuz);
+		}
+	}
+}
