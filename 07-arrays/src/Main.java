8101f32b76440de3d91485834e4e2ba78c113799
WHOREU <WHOREU@DESKTOP-NQ57GQJ>
2021-05-08 12:41:29 +0300

    MultiDimensionalArray

--- 07-arrays/src/Main.java
+++ 07-arrays/src/Main.java
@@ -17,5 +17,26 @@ public class Main {
 		for (String calisan : calisanlar) {
 			System.out.println(calisan);
 		}
+		System.out.println("\n");
+
+		// MultiDimensionalArray
+
+		String[][] sehirler = new String[3][3];
+		sehirler[0][0] = "Ýstanbul";
+		sehirler[0][1] = "Bursa";
+		sehirler[0][2] = "Bilecik";
+		sehirler[1][0] = "Ankara";
+		sehirler[1][1] = "Konya";
+		sehirler[1][2] = "Kayseri";
+		sehirler[2][0] = "Diyarbakýr";
+		sehirler[2][1] = "Þanlýurfa";
+		sehirler[2][2] = "Gaziantep";
+
+		for (int i = 0; i < 3; i++) {
+			System.out.println("------------------------");
+			for (int j = 0; j < 3; j++) {
+				System.out.println(i + ".satýr " + j  + ".sütun " +sehirler[i][j]);
+			}
+		}
 	}
 }
