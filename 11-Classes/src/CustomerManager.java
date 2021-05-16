baf3ba09d35f605cda43241236f04ab94a803080
oaltunorgu <ogulcan.altunorgu@gmail.com>
2021-05-16 11:08:59 +0300

    11-Classes

---
+++ 11-Classes/src/CustomerManager.java
@@ -0,0 +1,15 @@
+
+public class CustomerManager {
+	public void add() {
+		System.out.println("Müþteri bilgisi eklendi!");
+	}
+
+	public void delete() {
+		System.out.println("Müþteri bilgisi silindi!");
+	}
+
+	public void update() {
+		System.out.println("Müþteri bilgisi güncellendi!");
+	}
+
+}
