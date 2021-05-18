d5243fa7e1c03b893704b01309bab29ad539f77b
oaltunorgu <ogulcan.altunorgu@gmail.com>
2021-05-18 08:16:30 +0300

    15-inheritance

--- 15-inheritance/src/Main.java
+++ 15-inheritance/src/Main.java
@@ -4,9 +4,11 @@ public class Main {
 	public static void main(String[] args) {
 		Customer customer = new Customer();
 		Employee employee = new Employee();
-		//Yani ortak Person'da kullandýk. (extends Person)
+//		Yani ortak methodlarý Person'da kullandýk. (extends Person)
+//		customer.age = 15;
+//		employee.age = 20;
 
-		//2. örneðimizde ise EmployeeManager ve CustomerManager yarattýk ortak olanlarý add ve list'i PersonManager de kullandýk ve extend PersonManager diye kullandýk.
+//		2. örneðimizde ise EmployeeManager ve CustomerManager yarattýk ortak metodlarý add ve list'i PersonManager de kullandýk ve extend PersonManager diye kullandýk.
 		EmployeeManager employeeManager = new EmployeeManager();
 		CustomerManager customerManager = new CustomerManager();
 		
