4ead64a2a2c5c882656f6f51bfbffee8c90c5d55
oaltunorgu <ogulcan.altunorgu@gmail.com>
2021-06-13 17:59:05 +0300

    13-Constructor https://www.ogulcanaltunorgu.com/java-constructors-yapici-metodlar/

---
+++ 13-Constructor/src/Car.java
@@ -0,0 +1,53 @@
+public class Car {
+	private String color;
+	private String model;
+	private int doors;
+	private double engine;
+	
+	public Car() {
+		System.out.println("Parametresiz Constructor Çalýþtý!");
+	}
+	
+
+	public Car(String color, String model, int doors, double engine) {
+		this.color = color;
+		this.model = model;
+		this.doors = doors;
+		this.engine = engine;
+		System.out.println("Parametreli Constructor Çalýþtý!");
+	}
+
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public int getDoors() {
+		return doors;
+	}
+	public void setDoors(int doors) {
+		this.doors = doors;
+	}
+	public double getEngine() {
+		return engine;
+	}
+	public void setEngine(double engine) {
+		this.engine = engine;
+	}	
+}
+
+
+
+
+
+
+
+
+
---
+++ 13-Constructor/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+	public static void main(String[] args) {
+		Car car1 = new Car(); // Parametresiz
+
+		System.out.println("\n");
+
+		Car car2 = new Car("Mavi", "BMW", 4, 2.0); // Parametreli
+		System.out.println("Araba rengi: " + car2.getColor() + "\n" + "Araba modeli: " + car2.getModel() + "\n"
+				+ "Araba kapý sayýsý: " + car2.getDoors() + "\n" + "Araba motor: " + car2.getEngine());
+	}
+}
+
+
+
+
