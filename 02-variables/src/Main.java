99efd03c40f4427f14971981ed96074a09f57d2a
Oğulcan Altunörgü <53301333+OAltunorgu@users.noreply.github.com>
2021-10-16 21:12:01 +0300

    Utf-8

--- 02-variables/src/Main.java
+++ 02-variables/src/Main.java
@@ -2,10 +2,11 @@
 public class Main {
 
 	public static void main(String[] args) {
-		//Deðiþkenler
 		System.out.println("Sayi-1 : 10");
 		
 		int num = 10;
+		System.out.println("Sayi-1 : " + num);
+		
 		String mesaj = "Sayi-2 : ";
 		System.out.println(mesaj + num);
 	}
